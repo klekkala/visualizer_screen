dd0957efc280a7a5d67a0b3ee5249becd3bb5847
kiran4399 <kiran4399@gmail.com>
2016-07-19 17:55:58 +0800

    webcam support added

---
+++ camera.js
@@ -0,0 +1,11 @@
+(function() {
+
+var img = document.getElementById('container').firstChild;
+img.onload = function() {
+    if(img.height > img.width) {
+        img.height = '100%';
+        img.width = 'auto';
+    }
+};
+
+}());
